88aa32afce215b89a33ac38177cc066e6197c36a
Muhammad Hasnain Jabbar <muhammadhasnainjabbar@gmail.com>
2025-01-28 13:44:39 +0500

    Hasnain Uploaded

---
+++ a.js
@@ -0,0 +1,26 @@
+// function show(){
+//     document.getElementById('form').style.display='block'
+// }
+// function hide(){
+//     document.getElementById('form').style.display='none'
+// }
+function submit() {
+    document.getElementById('box').style.backgroundColor = document.getElementById('bgClr').value
+    document.getElementById('box').innerHTML = document.getElementById('txt').value
+    document.getElementById('box').style.fontSize = document.getElementById('fs').value
+    document.getElementById('box').style.fontWeight = document.getElementById('fw').value
+    document.getElementById('box').style.color = document.getElementById('txtClr').value
+}
+function show() {
+    let a = document.getElementById("cb3-8").checked;
+    if (a == false) {
+        document.getElementById('parent').style.display = 'none'
+    }
+    else {
+        document.getElementById('parent').style.display = 'grid'
+    }
+}
+
+
+
+
